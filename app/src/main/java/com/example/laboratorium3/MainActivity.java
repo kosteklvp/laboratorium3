f33532fb226c896a30a5a8db1af997f67416e106
Piotr Kostański <kostek.lvp@gmail.com>
2020-05-26 00:12:49 +0200

    po pierwszym podpnuckie

---
+++ app/src/main/java/com/example/laboratorium3/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.laboratorium3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button b;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        b = findViewById(R.id.call);
+
+        b.setOnClickListener(new View.OnClickListener() {
+
+            @SuppressLint("MissingPermission")
+            @Override
+            public void onClick(View v) {
+                Intent dialIntent = new Intent(Intent.ACTION_DIAL);
+                dialIntent.setData(Uri.parse("794947690"));
+                if (dialIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(dialIntent);
+                } else {
+                    Log.e("ERROR", "brak");
+                }
+            }
+        });
+    }
+
+
+}
